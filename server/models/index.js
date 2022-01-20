d7c8d330295663472e4e47b97bb2fecda1025818
Lawrence Rivales <lrivales@gmail.com>
2022-01-19 22:41:34 -0500

    simplified schema and created seeder file

--- server/models/index.js
+++ server/models/index.js
@@ -1,49 +1,5 @@
 const { Schema, Types, model } = require('mongoose');
 
-const categorySchema = (
-    {
-        categoryId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        categoryName: {
-            type: String,
-            unique: true,
-            default: 'None'
-        }
-    }
-);
-
-const itemSchema = (
-    {
-        itemId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        itemName: {
-            type: String,
-            required: true
-        },
-        brand: {
-            type: String
-        },
-        model: {
-            type: String
-        },
-        value: {
-            type: Number,
-            required: true
-        },
-        image: {
-            type: String
-        },
-        category: {
-            type: categorySchema,
-            default: 'None'
-        }
-    }
-);
-
 const userSchema = new Schema(
     {
         username: {
@@ -61,15 +17,40 @@ const userSchema = new Schema(
             required: true,
             minlength: 6
         },
-        categories: [categorySchema],
-        items: [itemSchema]
+        items: [
+            {
+                name: {
+                    type: String
+                },
+                description: {
+                    type: String
+                },
+                purchaseDate: {
+                    type: Date
+                },
+                value: {
+                    type: Number
+                },
+                image: {
+                    type: String
+                },
+                tags: [
+                    {
+                        tag: {
+                            type: String,
+                            unique: true
+                        }
+                    }   
+                ]
+            }
+        ]
+    },
+    {
+        toJSON: true,
+        versionKey: false
     }
 );
 
-categorySchema.set('toJSON', { getters: true, virtuals: false, versionKey: false });
-itemSchema.set('toJSON', { getters: true, virtuals: false, versionKey: false });
-userSchema.set('toJSON', { getters: true, virtuals: false, versionKey: false });
-
 const User = model('User', userSchema);
 
-module.exports = User;
+module.exports = { User };
--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -5,74 +5,23 @@ const db = require('../config/connection');
 const { User } = require('../models/index');
 
 db.once('open', async () => {
-    await db.dropDatabase();
-
-    const userCategories = [];
-    const images = [];
-    const userItems = [];
-    const userData = [];
-
-    // create fake categories
-    for (let i = 0; i < 10; i += 1) {
-        const categoryName = faker.random.word;
-
-        userCategories.push({ categoryName })
-    }
-
-    console.log('Categories: ' + userCategories);
-
-    // function for converting image to string using mongo-image-converter
-    const convertImage = async (img) => {
-        try {
-            const convertedImage = await Convert(img)
-            if (convertedImage) {
-                images.push({ convertedImage });
-                console.log('Converted Image: ' + convertedImage);
-            } else {
-                console.log('The file is not in format of image/jpeg or image/png');
-            }
-        } catch (error) {
-            console.warn(error);
-        }
-    }
-
-    // create fake images
-    for (let i = 0; i < 50; i += 1) {
-        const imageToConvert = faker.image.image;
-        console.log('Image to Convert: ' + imageToConvert);
-        convertImage(imageToConvert);
-    }
-
-    console.log('Images: ' + images);
-
-    // create fake items
-    for (let i =0; i < 50; i += 1) {
-        const randomImageIndex = Math.random(Math.floor() * images.length); //pick random image
-        const randomCategoryIndex = Math.random(Math.floor() * userCategories.length); //pick random category
-
-        const itemName = faker.commerce.product;
-        const brand = faker.company.companyName;
-        const model = faker.commerce.productName;
-        const value = faker.finance.amount;
-        const image = images[randomImageIndex];
-        const category = userCategories[randomCategoryIndex];
-
-        userItems.push({ itemName, brand, model, value, image, category })
-    }
-
-    console.log('Items: ' + userItems);
+    try {
+        await db.dropDatabase();
+        console.log('Database has been dropped.');
+    } catch (error) {
+        console.error(error);
+    };
 
     // create fake users
-    for (let i = 0; i < 5; i += 1) {
+    for (let user = 0; user < 5; user += 1) {
         const username = faker.internet.userName();
         const email = faker.internet.email(username);
-        const password = faker.internet.password();
-        const categories = userCategories;
-        const items = userItems;
-        userData.push({ username, email, password, categories, userItems });
+        const password = 'password';
+
+        const createdUser = await User.create({ username, email, password });
+        console.log(createdUser);
     }
-    
-    console.log('User Items: ' + userData);
-    
-    // const createdUsers = await User.collection.insertMany(userData);
+
+    console.log('Database has been seeded.');
+    process.exit(0);
 });
