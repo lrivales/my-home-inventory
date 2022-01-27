7b860e8e03c7b4d2ec4321b209dced21af2f5354
Lawrence Rivales <lrivales@gmail.com>
2022-01-27 10:00:06 -0500

    updated server.js for prod build

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const bodyParser = require('body-parser'); // required for mongo-image-converter
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
 const PORT = process.env.PORT || 3001;
@@ -29,6 +30,10 @@ app.use(bodyParser.json({limit: '16mb', extended: true})); // required for mongo
 app.use(bodyParser.urlencoded({limit: '16mb', extended: true})) // required for mongo-image-converter
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 db.once('open', () => {
   app.listen(PORT, () => {
